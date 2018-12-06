22c9def29031f5ff1daf525d932bbc9c68045db1
JohanKrogh <johan.krogh1@gmail.com>
2018-12-06 13:08:56 +0100

    Ved at lave en fscanf funktion. Samt en funktion der tæller linjer.
    Problemet er den ikke scanner i fscanf.
    Fordi den kommer ind i for-loop linje 97

--- main.c
+++ main.c
@@ -34,12 +34,18 @@ typedef struct student student;
 /* Declare fuction prototypes */
 
 int getLength(FILE *file);
+int linesInTextfile(FILE *file);
+void readFile(FILE *file, student studentList[], int lines);
+
 
 int main(void)
 {
 	FILE *inFP = fopen("input.txt","r");
 	int numStudents = getLength(inFP);
+  int lines = linesInTextfile(inFP);
 	student studentList[numStudents];
+  readFile(inFP, studentList, lines);
+  printf("%s\n", studentList[0].name);
 
     return 0;
 }
@@ -53,3 +59,60 @@ int getLength(FILE *file)
 
     return length;
 }
+
+int linesInTextfile(FILE *inFP){
+    int i = 0, count = 0;
+    if(inFP != NULL){
+
+        for (i = getc(inFP); i != EOF; i = getc(inFP)){ /*Tæller linjer indtil vi rammer EOF*/
+
+            if (i == '\n'){
+                count = count + 1;
+            }
+        }
+    }
+    return count;
+}
+
+void readFile(FILE *inFP, student studentList[], int lines){
+    int i = 0; /* læser filen efter guiden */
+    char roleOne[4];
+    char roleTwo[4];
+    char roleThree[4];
+
+    if(inFP == NULL)
+    {
+        printf("Couldn't open file\n");
+    }
+
+    if(inFP != NULL)
+    {
+      printf("File opended\n");
+    }
+    while (fscanf(inFP, "%*[^\n]"), i < lines){
+        i++;
+        printf("%d her\n", i);
+        if(i > 38)
+        {
+          for(; i < lines; i++)
+          {
+            printf("%d\n", i);
+            printf("hej2\n");
+              fscanf(inFP, " %[^,] %d, %s, %s, %s, %[^,], %[^,], %[^,], %[^,]", studentList[i].name, &studentList[i].ambitionLevel, roleOne, roleTwo, roleThree, studentList[i].doWant[0], studentList[i].doWant[1], studentList[i].doWant[2], studentList[i].notWant);
+              printf("%s ", studentList[i].name);
+          }
+      }
+    }
+}
+
+//     for(i; i < lines; i++)
+//     {
+//       printf("%d\n", i);
+//       printf("hej2\n");
+//         fscanf(inFP, " %[^,] %d, %s, %s, %s, %[^,], %[^,], %[^,], %[^,].",
+//                 studentList[i].name, &studentList[i].ambitionLevel, roleOne, roleTwo, roleThree,
+//                 studentList[i].doWant[0], studentList[i].doWant[1], studentList[i].doWant[2], studentList[i].notWant);
+//         printf("%s ", studentList[i].name);
+//     }
+//     }
+// }
