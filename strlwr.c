3bb644dd60f83271b2b666169f3d54f6b42d82c8
Adrian Plesner <adrian.plesner@gmail.com>
2018-12-07 09:49:29 +0100

    add strlwr c and header files

---
+++ strlwr.c
@@ -0,0 +1,4 @@
+//
+// Created by adrian plesner on 07/12/2018.
+//
+
---
+++ strlwr.h
@@ -0,0 +1,8 @@
+//
+// Created by adrian plesner on 07/12/2018.
+//
+
+#ifndef PROGRAM_STRLWR_H
+#define PROGRAM_STRLWR_H
+
+#endif //PROGRAM_STRLWR_H
