c17eec3baa6d3f4ce17adc53b8aaa17621f52aa9
Adrian Plesner <adrian.plesner@gmail.com>
2018-12-07 10:19:39 +0100

    fixed strlwr + readfile &

--- strlwr.c
+++ strlwr.c
@@ -1,4 +1,19 @@
 //
 // Created by adrian plesner on 07/12/2018.
 //
+#include "strlwr.h"
+#include <string.h>
+#include <ctype.h>
 
+char *strlwr(char* str)
+{
+    int len = strlen(str);
+    for(int i = 0; i < len; i++)
+    {
+        if(isupper((int)str[i]))
+        {
+            str[i] = (char)tolower((int)str[i]);
+        }
+    }
+    return str;
+}
--- strlwr.h
+++ strlwr.h
@@ -5,4 +5,6 @@
 #ifndef PROGRAM_STRLWR_H
 #define PROGRAM_STRLWR_H
 
+char *strlwr(char*);
+
 #endif //PROGRAM_STRLWR_H
