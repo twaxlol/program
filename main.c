4330f291116a5cae6f1a37a3633f651e653c7970
Enyumin <chrislddk@hotmail.com>
2018-12-07 10:34:39 +0100

    j added...?

--- main.c
+++ main.c
@@ -82,7 +82,7 @@ void readFile(student studentList[], int rolesCount[9][2], int numberOfStudents)
 {
     FILE *inFP = fopen("input.txt","r");
     char rolesStr[MAX_ROLES][4];
-
+    int j;
 
     if(inFP == NULL)
     {
@@ -108,7 +108,7 @@ void readFile(student studentList[], int rolesCount[9][2], int numberOfStudents)
             /*printf("%s %d %s %s %s %s %s %s %s\n", studentList[i].name, studentList[i].ambitionLevel, rolesStr[0],
                         rolesStr[1], rolesStr[2],x studentList[i].doWant[0], studentList[i].doWant[1],
                         studentList[i].doWant[2], studentList[i].notWant);*/
-            for(int j = 0; j < MAX_ROLES; j++)
+            for(j = 0; j < MAX_ROLES; j++)
             {
 
                 if(strcmp(strlwr(rolesStr[j]), "iga") == 0)
